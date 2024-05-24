ff5cb21bb564e06447bda71f8d0f67ff86ee0621
Louis Besancon <louis.besancon2@gmail.com>
2024-05-24 10:29:58 +0200

    rajout #

--- hello.py
+++ hello.py
@@ -28,11 +28,12 @@ def user(name):
 
 
 #create custom error pages
-
+#invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template("404.html"), 404
 
+#Internal Server Error thing
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
